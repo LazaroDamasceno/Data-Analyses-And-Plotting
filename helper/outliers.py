2b0d9b7c8f41f2b60bf22dcea86c173bb3e3461d
LazaroDamasceno <lazarolawrence2@gmail.com>
2025-11-15 22:26:44 -0300

    Alterou o arquivo PY outliers

--- helper/outliers.py
+++ helper/outliers.py
@@ -1,11 +1,6 @@
 
-def is_outlier(q1, q3, number):
-
+def is_outlier(q1: int, q3: int, number: int) -> bool:
     iqr = q3 - q1
     lower_limit = q1 - 1.5*iqr
     upper_limit = q3 + 1.5*iqr
-
-    if (number < lower_limit or number > upper_limit):
-        return True
-    else:
-        return False
+    return number < lower_limit or number > upper_limit
